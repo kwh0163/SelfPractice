5c312dec95db99dc0c0bb8870e4954e49f067369
우협 김 <kwh0163@gmail.com>
2022-12-31 10:27:23 +0900

    a

--- SelfPractice/SelfPractice.cpp
+++ SelfPractice/SelfPractice.cpp
@@ -1,8 +1,12 @@
 ﻿#include<iostream>
 #include<vector>
 #include<array>
+#include<memory>
 
 using namespace std;
+
+
+
 
 int main() {
 	//vector  <vector>헤더파일 사용
@@ -40,6 +44,33 @@ int main() {
 
 	//배열 복사 가능
 	array<int, 3> STD_B = STD_A;
+	*/
+
+	//스마트 포인터
+	/*
+	//일반 포인터는 딜리트 하지않으면 메모리 누수가 생김
+	int* RawA = new int();
+	delete RawA;
+
+	//unique_ptr
+	//unique 포인터는 힙에 있는 데이터를 가리키는 포인터가 한개밖에 지정되지 않는다. 
+	unique_ptr<int> U_A = make_unique<int>();
+	//그래서 카피는 안되지만 무브는 가능
+	unique_ptr<int> U_B = move(U_A);
+
+	//shared_ptr
+	//shared 포인터는 서로 공유가 가능하지만 포인터들이 서로 사이클될 수가 있다. 
+	shared_ptr<int> S_A = make_shared<int>();
+	//복사 가능
+	shared_ptr<int> S_B = S_A;
+
+	//weak_ptr
+	//weak 포인터는 카운트에 영향을 미치지 않는 포인터이다.
+	weak_ptr<int> W_A;
+	shared_ptr<int> S_P = make_shared<int>();
+	//S_P를 가리키는 포인터가 두개지만 정작 카운트되는 수는 1개이다.
+	W_A = S_P;
+	cout << "S_P의 카운트 : " << S_P.use_count() << endl;
 	*/
 
 
